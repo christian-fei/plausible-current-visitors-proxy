d11ba56bcd9db5eb4a59c4c37ca4f0b7743281cc
Christian Fei <crifei93@gmail.com>
2020-10-20 16:44:39 +0200

    before after each in test

--- index.test.js
+++ index.test.js
@@ -1,22 +1,22 @@
-const { test } = require('m.test')
+const { test, beforeEach, afterEach } = require('m.test')
 const nock = require('nock')
 const { ok, strictEqual } = require('assert')
 const listen = require('.')
 
 const http = require('http')
 
+let server
+
 test('server proxies through to plausible.io api', function (done) {
   const scope = nock('https://plausible.io')
     .get('/api/stats/cri.dev/current-visitors')
     .reply(200, '123')
-  const server = listen(0, 'cri.dev')
   const address = `http://127.0.0.1:${server.address().port}`
 
   ok(server)
   ok(address)
   console.log('making request to', address)
   http.get(address, (res) => {
-    server.close()
     res.on('data', function (chunk) {
       const stringified = chunk.toString()
       console.log('received chunk', stringified)
@@ -26,4 +26,11 @@ test('server proxies through to plausible.io api', function (done) {
     scope.done()
     done()
   })
+})
+
+beforeEach(() => {
+  server = listen(0, 'cri.dev')
+})
+afterEach(() => {
+  server.close()
 })
